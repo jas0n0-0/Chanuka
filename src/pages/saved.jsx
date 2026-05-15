8a027595d4de0350a024a83825863a5e8431645f
jason_mumo <jason.mumo@student.moringaschool.com>
2026-05-15 13:32:54 +0300

    navbar completed

---
+++ src/pages/saved.jsx
@@ -0,0 +1,9 @@
+function Saved (){
+    return(
+        <>
+        <h1>Bookmarked</h1>
+        </>
+    )
+}
+
+export default Saved;
---
+++ src/pages/trending.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Trending() {
+    return(
+        <>
+        <h1>trending</h1>
+        </>
+    )
+}
+
+
+export default Trending;
